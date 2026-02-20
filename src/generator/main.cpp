5b8664524299e18063bea698eab6ec4e4347491b
Duarte Escairo <a106936@alunos.uminho.pt>
2026-02-20 11:22:07 +0000

    A minha contribuição

--- src/generator/main.cpp
+++ src/generator/main.cpp
@@ -1,10 +1,11 @@
 #include "generator/main.hpp"
+using namespace std;
 
 // Armazena os vértices gerados no ficheiro
-void storeVertices(const std::vector<float>& vertices, const std::string& file) {
-    std::ofstream out(file);
+void storeVertices(const vector<float>& vertices, const string& file) {
+    ofstream out(file);
     if (!out.is_open()) {
-        std::cerr << "ERRO: Não foi possível abrir o ficheiro.";
+        cerr << "ERRO: Não foi possível abrir o ficheiro.";
         return;
     }
     for (size_t i = 0; i < vertices.size(); i += 3) {
@@ -15,28 +16,28 @@ void storeVertices(const std::vector<float>& vertices, const std::string& file) 
 
 int main(int argc, char** argv) {
     if (argc < 5) {
-        std::cerr << "ERRO: Parâmetros insuficientes.";
+        cerr << "ERRO: Parâmetros insuficientes.";
         return 1; 
     }
 
-	std::vector <float> vertices;
-    const std::string file = argv[argc - 1];
-    const std::string figure = argv[1];
+	vector <float> vertices;
+    const string file = argv[argc - 1];
+    const string figure = argv[1];
 
     if (figure == "plane" && argc == 5) {
-        vertices = generatePlane(std::stof(argv[2]), std::stoi(argv[3]));
+        vertices = generatePlane(stof(argv[2]), stoi(argv[3]));
     }
     else if (figure == "box" && argc == 5) {
-        vertices = generateBox(std::stof(argv[2]), std::stoi(argv[3]));
+        vertices = generateBox(stof(argv[2]), stoi(argv[3]));
     }
     else if (figure == "sphere" && argc == 6) {
-        //vertices = generateSphere(std::stof(argv[2]), std::stoi(argv[3]), std::stoi(argv[4])); 
+        //vertices = generateSphere(stof(argv[2]), stoi(argv[3]), stoi(argv[4])); 
     }
     else if (figure == "cone" && argc == 7) {
-        //vertices = generateCone(std::stof(argv[2]), std::stof(argv[3]), std::stoi(argv[4]), std::stoi(argv[5]));
+        //vertices = generateCone(stof(argv[2]), stof(argv[3]), stoi(argv[4]), stoi(argv[5]));
     }
     else {
-        std::cerr << "ERRO: Parâmetros inválidos.\nUSAGE: generator <primitive> <params> <dest file>";
+        cerr << "ERRO: Parâmetros inválidos.\nUSAGE: generator <primitive> <params> <dest file>";
         return 1;
     }
 
