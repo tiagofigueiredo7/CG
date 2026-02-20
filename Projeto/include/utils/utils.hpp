c08da6a8b473522ad3344f787cd313daae8b0ea9
a106932 <a106932@alunos.uminho.pt>
2026-02-20 23:13:41 +0000

    melhoria na organização do projeto

--- Projeto/include/utils/utils.hpp
+++ Projeto/include/utils/utils.hpp
@@ -7,6 +7,4 @@ using namespace std;
 
 namespace utils {
     void addTriangle(vector<float>& v, float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3);
-    
-    string getAttribute(const string& line, const string& attr);
 }
