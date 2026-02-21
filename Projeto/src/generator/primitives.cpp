fc7e1a6310645b54eb71edfbb0f5b1c63b226ae7
Duarte Escairo <a106936@alunos.uminho.pt>
2026-02-21 15:48:36 +0000

    Cone

--- Projeto/src/generator/primitives.cpp
+++ Projeto/src/generator/primitives.cpp
@@ -161,5 +161,52 @@ vector <float> generateSphere(float radius, int slices, int stacks){
 
 // Cria os vértices de um cone
 vector <float> generateCone(float radius, float height, int slices, int stacks) {
-	return vector <float>();
+	vector <float> vertices;
+
+	float a = 2*M_PI/slices;
+	float b = height/stacks;
+
+	for (int i = 0 ; i < stacks ; i++ ){
+
+		float b1 = i * b;
+	   	float b2 = (i+1) * b;
+
+		for (int j = 0 ; j < slices ; j++ ){
+
+			float a1 = j * a;
+			float a2 = (j + 1) * a;
+
+			float r1 = radius * (height - b1) / height;
+			float r2 = radius * (height - b2) / height;
+
+			// CANTO SUPERIOR ESQUERDO
+			float x1 = r2 * sin(a1);
+			float y1 = b2;
+			float z1 = r2 * cos(a1);
+
+			// CANTO SUPERIOR DIREITO
+			float x2 = r2 * sin(a2);
+			float y2 = b2;
+			float z2 = r2 * cos(a2);
+
+			// CANTO INFERIOR ESQUERDO
+			float x3 = r1 * sin(a1);
+			float y3 = b1;
+			float z3 = r1 * cos(a1);
+
+			// CANTO INFERIOR DIREITO
+			float x4 = r1 * sin(a2);
+			float y4 = b1;
+			float z4 = r1 * cos(a2);
+
+			addTriangle(vertices, x1, y1, z1, x3, y3, z3, x4, y4, z4);
+			addTriangle(vertices, x1, y1, z1, x4, y4, z4, x2, y2, z2);
+			
+			
+		}
+
+	}
+
+
+	return vertices;
 }
