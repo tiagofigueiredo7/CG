36c3648b1df1f2dd2cca6f9595d117e9d52c04b6
Luís Soares <a106932@alunos.uminho.pt>
2026-03-29 15:56:08 +0100

    alteração temporaria

--- projeto/src/engine/engine.cpp
+++ projeto/src/engine/engine.cpp
@@ -49,7 +49,7 @@ void renderGroup(Group& g){
 			glScalef(s->getX(), s->getY(), s->getZ());
 		}
 		else if (Curve* c = dynamic_cast<Curve*>(t)){
-			c->renderCatmullRomCurve();
+			//c->renderCatmullRomCurve();
 			float elapsedSeconds = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
 			float time = c->getTime(), gt = fmod(elapsedSeconds,time) / time;		
 			float pos[3], deriv[3];
